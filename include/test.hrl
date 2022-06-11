54a9da2a65f182e0a8ecd857e3e4540fe7b268a1
alekras <krasnop@bellsouth.net>
2022-06-10 23:51:37 -0400

    Update copyright lines.

--- include/test.hrl
+++ include/test.hrl
@@ -1,5 +1,5 @@
 %%
-%% Copyright (C) 2016-2016 by krasnop@bellsouth.net (Alexei Krasnopolski)
+%% Copyright (C) 2015-2022 by krasnop@bellsouth.net (Alexei Krasnopolski)
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -30,23 +30,19 @@
 	-define(TEST_SERVER_PORT, 18883). %% Erlang
 	%-define(TEST_SERVER_PORT, 2883). %% Mosquitto
 	%-define(TEST_SERVER_PORT, 1883). %% RabbitMQ
-	-define(TEST_REST_SERVER_URL, "http://" ++ ?TEST_SERVER_HOST_NAME ++ ":8880").
 -elif(?CONN_TYPE == ssl).
 %%%%%%%%%%%%% SSL/TSL socket test %%%%%%%%%%%%%%%%% 
 	-define(TEST_CONN_TYPE, {conn_type, ssl}). %% TSL/SSL for client
 	-define(TEST_SERVER_PORT, 18483). %% Erlang TSL
 	%-define(TEST_SERVER_PORT, 2884). %% Mosquitto TSL
-	-define(TEST_REST_SERVER_URL, "http://" ++ ?TEST_SERVER_HOST_NAME ++ ":8880").
 -elif(?CONN_TYPE == ws).
 %%%%%%%%%%%%% Clear WEB socket test %%%%%%%%%%%%%%%%% 
 	-define(TEST_CONN_TYPE, {conn_type, web_socket}). %% Web socket connection for client
 	-define(TEST_SERVER_PORT, 8880). %% Erlang WEBSocket
-	-define(TEST_REST_SERVER_URL, "http://" ++ ?TEST_SERVER_HOST_NAME ++ ":8880").
 -elif(?CONN_TYPE == wss).
 %%%%%%%%%%%%% SSL/TLS WEB socket test %%%%%%%%%%%%%%%%% 
 	-define(TEST_CONN_TYPE, {conn_type, web_sec_socket}). %% Web socket connection for client
 	-define(TEST_SERVER_PORT, 8443). %% Erlang WEBSocket
-	-define(TEST_REST_SERVER_URL, "http://" ++ ?TEST_SERVER_HOST_NAME ++ ":8880").
 -endif.
 
 -define(debug_Msg(S),
